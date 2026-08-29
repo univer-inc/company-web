06f03fa0ad2a64ddb551ed50b7f701ec3e14d488
ogwtkhr <7401675+ogwtkhr@users.noreply.github.com>
2026-08-29 13:39:23 +0900

    お知らせアップデート

--- src/app/_sections/News.tsx
+++ src/app/_sections/News.tsx
@@ -1,6 +1,7 @@
 import tw from 'twin.macro';
 import { Section } from '@/components/part/Section';
 import { Heading } from '@/components/part/Heading';
+import { newsData } from '@/data/news';
 
 import OriginalNoteLogo from '@/components/icon/note.svg';
 import { Button, ButtonContainer } from '@/components/part/Button';
@@ -10,16 +11,20 @@ export const News = () => {
     <Section id="news">
       <Heading en="NEWS" ja="お知らせ" />
       <NewsList>
-        <NewsTitle>2024.9.14</NewsTitle>
-        <NewsBody>
-          <NewsLink
-            href="https://prtimes.jp/main/html/rd/p/000000001.000147910.html"
-            target="_blank"
-          >
-            株式会社ウニベル、大学生が地方と都市を自由に行き来しながら新たな学びと挑戦の機会を広げる支援事業「Campus
-            Everywhere 構想」を発表
-          </NewsLink>
-        </NewsBody>
+        {newsData.map((item) => (
+          <NewsRow key={item.date}>
+            <NewsTitle>{item.date}</NewsTitle>
+            <NewsBody>
+              {item.url ? (
+                <NewsLink href={item.url} target="_blank">
+                  {item.title}
+                </NewsLink>
+              ) : (
+                <NewsText>{item.title}</NewsText>
+              )}
+            </NewsBody>
+          </NewsRow>
+        ))}
       </NewsList>
       <ButtonLead>公式noteで最新情報を発信中！</ButtonLead>
       <ButtonContainer>
@@ -44,27 +49,40 @@ const ButtonLead = tw.p`
   text-center
 `;
 
-const NewsList = tw.dl`
+const NewsList = tw.div`
   flex
-  gap-12
-  items-center
+  flex-col
+  gap-6
   mx-12
   my-16
-  sm-df:flex-col
-  sm-df:items-start
-  sm-df:gap-6
   sm-df:mx-3
 `;
 
+const NewsRow = tw.dl`
+  flex
+  gap-12
+  items-start
+  sm-df:flex-col
+  sm-df:gap-1
+`;
+
 const NewsTitle = tw.dt`
+  shrink-0
+  min-w-[9ch]
+  tabular-nums
   text-xl
   font-bold
 `;
 
 const NewsBody = tw.dd`
+  flex-1
   text-lg
 `;
 
 const NewsLink = tw.a`
   hover:underline
+`;
+
+const NewsText = tw.span`
+  text-lg
 `;
